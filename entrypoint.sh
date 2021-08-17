353222cc07ce9a5eddf60213aeb3f28689b75075
sam bacha <sam@manifoldfinance.com>
2021-08-17 08:56:09 -0700

    update
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ export NEXT_TELEMETRY_DISABLED=1
 echo "Configuring RPC Connections"
 DEFAULT_RPC_URI='https://api.sushirelay.com/v1'
 
+yarn install
+yarn run lingui
+npx next build
+
 if [[ -n "${MANIFOLD_FINANCE_RPC_URI}" ]]; then
 echo "Replacing default RPC URI value with '${MANIFOLD_FINANCE_RPC_URI}' ..."
 find .next/static/chunks .next/server/chunks -type f -exec sed -i -e "s|${DEFAULT_RPC_URI}|${MANIFOLD_FINANCE_RPC_URI}|g" {} \;
