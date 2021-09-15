63f5a346792a282daf07e48db12c9ab58397a6ae
sam bacha <sam@manifoldfinance.com>
2021-09-15 05:51:48 -0700

    fix deploy

--- build_env.sh
+++ build_env.sh
@@ -18,6 +18,3 @@ if [[ -n "${MANIFOLD_FINANCE_RPC_URI}" ]]; then
     echo "Replacing default RPC URI value with '${MANIFOLD_FINANCE_RPC_URI}' ..."
     find .next/static/chunks .next/server/chunks -type f -exec sed -i -e "s|${DEFAULT_RPC_URI}|${MANIFOLD_FINANCE_RPC_URI}|g" {} \;
 fi
-
-echo "Executing NextJS server..."
-yarn start
