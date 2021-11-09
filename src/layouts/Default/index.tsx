97745098ee9249151a3c018603aada6710a27128
IvoPaunov <ivo.paunov@gmail.com>
2021-11-09 15:56:23 +0200

    remove header and layout stuff

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -7,9 +7,12 @@ import Popups from '../../components/Popups'
 const Layout = ({ children, banner = undefined }) => {
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0">
-      {banner && <Banner />}
-      <Header />
-      <Main>{children}</Main>
+      {/* {banner && <Banner />} */}
+   
+      <Main>
+        <Header />
+        {children}
+      </Main>
       <Popups />
       {/* <Footer /> */}
     </div>
