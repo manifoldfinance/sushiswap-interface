ba0baf47495cce564822d712f8f76e17dee9751a
IvoPaunov <ivo.paunov@gmail.com>
2021-11-15 21:11:24 +0200

    fix: remove header

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,16 +1,16 @@
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
+// import Footer from '../../components/Footer'
+// import Header from '../../components/Header'
 import Main from '../../components/Main'
-import Banner from '../../components/Banner'
+// import Banner from '../../components/Banner'
 import Popups from '../../components/Popups'
 
 const Layout = ({ children, banner = undefined }) => {
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0">
       {/* {banner && <Banner />} */}
-   
+
       <Main>
-        <Header />
+        {/* <Header /> */}
         {children}
       </Main>
       <Popups />
