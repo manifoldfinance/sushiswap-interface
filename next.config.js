f48a488caf53208f41d2f22edfd1e8213b8a93ac
sam bacha <sam@manifoldfinance.com>
2021-11-19 03:37:32 -0800

    feat(sentry): init

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const { withSentryConfig } = require('@sentry/nextjs');
 // @ts-check
 /**
  * @type {import('next').NextConfig}
@@ -163,3 +164,20 @@ module.exports = withBundleAnalyzer(
 
 // Don't delete this console log, useful to see the config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
