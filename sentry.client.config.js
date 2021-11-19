507b9bd719864d50670ed09d624c4ad7226820a7
sam bacha <sam@manifoldfinance.com>
2021-11-19 05:02:31 -0800

    test(sentry): tracing

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,12 +3,16 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
+import { Integrations as TracingIntegrations } from "@sentry/tracing";
+
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://b3d1f140b36f441da22791ca98210c95@o1029417.ingest.sentry.io/6052500",
   // Adjust this value in production, or use tracesSampler for greater control
+  integrations: [new TracingIntegrations.BrowserTracing()],
+
   tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
