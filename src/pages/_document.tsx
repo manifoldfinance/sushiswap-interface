36ed7d7093140f0d7b5c1cfdd37e89f3b03e7ab4
Lint Action <lint-action@samuelmeuli.com>
2022-03-09 12:13:00 +0000

    style: code style issues with ESLint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,67 +1,32 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // @note there is no actual error here, the rule thinks _document.page.tsx is different from _document.tsx
-import Document, { Html, Head, Main, NextScript } from 'next/document'; 
-import React, { ReactElement } from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React, { ReactElement } from 'react'
 const APP_NAME = 'SushiSwap'
 const APP_DESCRIPTION = 'Swap, yield, lend, borrow, leverage, limit, launch all on one community driven ecosystem'
 
 class MyDocument extends Document {
-    render(): ReactElement {
+  render(): ReactElement {
     return (
-        <Html>
+      <Html>
         <Head>
           <meta name="application-name" content={APP_NAME} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content={APP_NAME} />
           <meta name="description" content={APP_DESCRIPTION} />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#0993ec" />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/icons/icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="96x96"
-            href="/icons/icon-96x96.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="128x128"
-            href="/icons/icon-128x128.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/icons/icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/icon-152x152.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="192x192"
-            href="/icons/icon-192x192.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="384x384"
-            href="/icons/icon-384x384.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="512x512"
-            href="/icons/icon-512x512.png"
-          />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="96x96" href="/icons/icon-96x96.png" />
+          <link rel="apple-touch-icon" sizes="128x128" href="/icons/icon-128x128.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
+          <link rel="apple-touch-icon" sizes="384x384" href="/icons/icon-384x384.png" />
+          <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
 
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" />
@@ -71,7 +36,7 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
@@ -101,24 +66,21 @@ MyDocument.getInitialProps = async (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+    })
 
-  const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  }
+}
 
-export default MyDocument;
+export default MyDocument
