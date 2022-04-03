1082ddecdfa1ae7b458e25615ff5ef41d18756bd
sam bacha <sam@manifoldfinance.com>
2022-04-03 00:05:59 -0700

    chore(api): remove healthcheck

--- src/pages/api/healthcheck.ts
+++
@@ -1,9 +0,0 @@
-import { withSentry } from '@sentry/nextjs'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = async function (_req: NextApiRequest, res: NextApiResponse) {
-  const response = { status: 200, message: 'Everything is okay!' }
-  res.status(200).json(response)
-}
-
-export default withSentry(handler)
