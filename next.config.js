2f39d343de1e5f47487c185665b1b81391b7574b
sam bacha <sam@manifoldfinance.com>
2022-05-22 17:26:24 -0700

    fix(nextjs): webpack bundling

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const path = require('path');
 
 const { withSentryConfig } = require('@sentry/nextjs');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
@@ -25,14 +26,54 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
+  silent: false, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
+// @ts-ignore
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GIT_COMMIT_SHA,
+  VERCEL_BITBUCKET_COMMIT_SHA,
+  NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+} = process.env
+
+process.env.SENTRY_DSN = SENTRY_DSN
+const basePath = ''
+
 
 const nextConfig = {
-  webpack: (config) => {
+  poweredByHeader: false,
+  productionBrowserSourceMaps: true,
+  swcMinify: false,
+  reactStrictMode: true,
+  env: {
+    // Make the COMMIT_SHA available to the client so that Sentry events can be
+    // marked for the release they belong to. It may be undefined if running
+    // outside of Vercel
+    NEXT_PUBLIC_COMMIT_SHA: VERCEL_GIT_COMMIT_SHA,
+  },
+  webpack:(config, options) => {
+    // Ensure Webpack replaces @sentry/node imports with @sentry/browser building the browser's bundle
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    }
+
+    // Define an environment variable so source code can check whether or not
+    // it's running on the server so we can correctly initialize Sentry
+    config.plugins.push(
+      new options.webpack.DefinePlugin({
+        'process.env.NEXT_IS_SERVER': JSON.stringify(
+          options.isServer.toString()
+        ),
+      })
+    )
     config.module.rules = [
       ...config.module.rules,
       {
@@ -41,15 +82,29 @@ const nextConfig = {
       },
     ]
 
+    if (
+ //     SENTRY_DSN &&
+ //     SENTRY_ORG &&
+ //     SENTRY_PROJECT &&
+ //     SENTRY_AUTH_TOKEN &&
+      VERCEL_GIT_COMMIT_SHA &&
+      NODE_ENV === 'production'
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          stripPrefix: ['webpack://_N_E/'],
+          urlPrefix: `~${basePath}/_next`,
+          release: VERCEL_GIT_COMMIT_SHA,
+        })
+      )
+    }
     return config
   },
   // experimental: {
   //   nextScriptWorkers: true,
   // },
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-  swcMinify: false,
-  reactStrictMode: true,
   // pwa: {
   //   dest: 'public',
   //   runtimeCaching,
@@ -124,6 +179,14 @@ const nextConfig = {
       },
     ]
   },
+  async headeres() {
+    return [
+      {
+        source: '/*',
+        headers: [{ key: 'Web-Build', value: process.env.VERCEL_GIT_COMMIT_SHA }]
+      }
+    ];
+  },
   i18n: {
     localeDetection: true,
     locales,
@@ -143,6 +206,10 @@ const nextConfig = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(withBundleAnalyzer(nextConfig), sentryWebpackPluginOptions);
-
 // Don't delete this console log, useful to see the config in Vercel deployments
-console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+console.log('process.env.VERCEL_GIT_COMMIT_SHA: ', process.env.VERCEL_GIT_COMMIT_SHA);
+// VERCEL_GIT_REPO_ID
+// NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
+console.log('process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: ', process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA);
+//console.log('process.env.GIT_COMMIT_SHA: ', process.env.GIT_COMMIT_SHA);
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
