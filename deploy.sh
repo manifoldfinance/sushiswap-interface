b8fd6df518822efde2262c874e894993a792835d
sam bacha <sam@manifoldfinance.com>
2022-05-30 05:24:53 -0700

    fix(deploy): vercel

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ check_mark="\033[1;32m✔\033[0m"
 header() { echo -e "\n\033[1m$1\033[0m"; }
 
 header "Nextjs Production Deployment\n"
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+echo "✅ - Build can proceed"
 echo "Enviornment configuration..."
 export NEXT_TELEMETRY_DISABLED=1
 export CI=''
@@ -15,15 +19,18 @@ echo $NEXT_TELEMETRY_DISABLED
 echo -e "\033[1A ${check_mark} Configured enviornment ... passing"
 sleep 1
 echo "Installing..."
-yarn install --frozen-lockfile
+yarn install
+npx lingui extract --clean --overwrite && lingui compile
 echo -e "\033[1A ${check_mark} Install ... done"
-echo "Building..."
-node --expose-gc node_modules/.bin/next build
+echo "Compiling ..."
+echo -e "\033[1A ${check_mark} Compiling output ... done"
+npx next build
 sleep 1
 node atomic.cjs
-echo -e "\033[1A ${check_mark} Building ... done"
+echo -e "\033[1A ${check_mark} Artificat Written ... done"
+echo -e "\033[1A ${check_mark} Building Output ... done"
 
-
+echo "✅ - Deployment Passing..."
+echo "📣 ✨ oh yea ✨ 📣 "
 echo -e " \033[1mDeployment build finished\033[0m"
-
 exit 0
