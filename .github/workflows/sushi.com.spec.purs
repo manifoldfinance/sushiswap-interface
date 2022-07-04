e2f2e77be21fb3856d7c5637cc58c72b6c88b0a9
sam bacha <sam@manifoldfinance.com>
2022-07-03 23:33:19 -0700

    feat(testing): init spec

---
+++ .github/workflows/sushi.com.spec.purs
@@ -0,0 +1,23 @@
+module Spec where
+
+import Quickstrom
+import Data.Foldable (any)
+import Data.Maybe (maybe)
+import Data.Tuple (Tuple(..))
+import Data.String.CodeUnits (contains)
+import Data.String.Pattern (Pattern(..))
+
+readyWhen = "body"
+
+actions = [ 
+  Tuple 1 (Single $ Click "a[href^='/']")
+]
+
+patterns = [ "404", "500" ]
+
+hasErrorCode = any (\x -> contains (Pattern x) pageText) patterns
+
+pageText :: String
+pageText = maybe "" _.textContent (queryOne "body" { textContent })
+
+proposition = always (not hasErrorCode)
