369c253b9ed653c5e6a2c5892a516d9dab7ab527
s bacha <sam@manifoldfinance.com>
2022-08-19 09:55:56 -0700

    router(trade): types

--- src/functions/getTradeVersion.ts
+++ src/functions/getTradeVersion.ts
@@ -2,9 +2,10 @@ import { Currency, Trade as V2Trade, TradeType, TradeVersion } from '@sushiswap/
 import { Trade as V3Trade } from '@sushiswap/trident-sdk'
 
 export function getTradeVersion(
-  trade?: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType>
+  trade?: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType> // | VXTrade<Currency, Currency, TradeType>
 ): TradeVersion | undefined {
   if (!trade) return undefined
   if (trade instanceof V2Trade) return TradeVersion.V2TRADE
+//  if (trade instanceof VXTrade) return TradeVersion.VXTRADE
   return TradeVersion.V3TRADE
 }
